fe9090ffd52a78707b1c140ac797f88f15ee4fee
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-06 08:57:14 +0330

    Add auto-session for better session management

---
+++ lua/plugins/auto-session.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "rmagatti/auto-session",
+    keys = {
+      { "<leader>qr", "<cmd>SessionRestore<cr>", desc = "auto-session restore" },
+      { "<leader>qs", "<cmd>SessionSave<cr>", desc = "auto-session save" },
+      { "<leader>qf", ":lua require('auto-session.session-lens').search_session()<CR>", desc = "auto-session lens" },
+    },
+
+    cmd = { "SessionSave", "SessionRestore" },
+
+    opts = {
+      -- log_level = "error",
+      auto_session_enabled = true,
+      auto_save_enabled = true,
+      session_lens = {
+        buftypes_to_ignore = {}, -- list of buffer types what should not be deleted from current session
+        load_on_setup = true,
+        theme_conf = { border = true },
+        previewer = false,
+      },
+    },
+  },
+}
