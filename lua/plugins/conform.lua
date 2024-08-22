eede33b74bc0072a701651d8fef2fc3dd0b7e89f
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-22 16:25:50 +0330

    Fix conform.nvim plugin declaration table

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,16 +1,18 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      c = { "clang-format" },
-      javascript = { "biome" },
-      json = { "biome" },
-      lua = { "stylua" },
-      python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
-      sh = { "shellharden", "shfmt" },
-      typescript = { "biome" },
-      typescriptreact = { "biome" },
-      zsh = { "shellharden", "shfmt" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        c = { "clang-format" },
+        javascript = { "biome" },
+        json = { "biome" },
+        lua = { "stylua" },
+        python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
+        sh = { "shellharden", "shfmt" },
+        typescript = { "biome" },
+        typescriptreact = { "biome" },
+        zsh = { "shellharden", "shfmt" },
+      },
     },
   },
 }
