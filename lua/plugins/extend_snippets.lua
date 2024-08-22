02e426aa37ef8f472ecea291f81eb133f9bf8278
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-22 16:26:10 +0330

    Add regular html snippets to htmldjango files

--- lua/plugins/extend_snippets.lua
+++ lua/plugins/extend_snippets.lua
@@ -2,7 +2,7 @@ return {
   {
     "rafamadriz/friendly-snippets",
     opts = function()
-      -- require("luasnip").filetype_extend("htmldjango", { "html" })
+      require("luasnip").filetype_extend("htmldjango", { "html" })
       require("luasnip").filetype_extend("python", { "django" })
       require("luasnip").filetype_extend("typescriptreact", { "html" })
     end,
