74decbcecb1eba86c76a6f3867fbb211ca55a3da
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-22 16:27:56 +0330

    Add htmldjango to the list of html lsp filetypes

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,6 +3,10 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       inlay_hints = { enabled = false },
+
+      servers = {
+        html = { filetypes = { "html", "htmldjango", "templ" } },
+      },
     },
   },
 }
