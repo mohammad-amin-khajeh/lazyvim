7a73c1f8c55d455a63c8e0d6298d281fe00be3e3
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-28 09:02:55 +0330

    Add zen-mode.nvim

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle zen mode" } },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+}
