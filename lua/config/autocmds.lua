abcf4e4f380c78565ce50acd9b66edb92331d9e5
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-28 08:35:39 +0330

    Add some useful autocmds for working with zola

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,27 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Copy the photo from clipboard to the current file's directory
+vim.api.nvim_create_user_command("ZolaPic", function()
+  local filename = vim.fn.input({ prompt = "enter the filename without an extension: " })
+  local cwd = vim.fn.expand("%:p:h")
+
+  os.execute("xclip -sel c -t image/png -o > " .. cwd .. "/" .. filename .. ".png")
+end, {})
+
+-- fill in frontmatter boilerplate for pages
+vim.api.nvim_create_user_command("ZolaFrontmatter", function()
+  local current_date = os.date("%Y-%m-%d")
+  vim.api.nvim_buf_set_lines(0, 0, 0, false, {
+    "+++",
+    "title = ''",
+    "description = ''",
+    "date = " .. '"' .. current_date .. '"',
+    "[extra]",
+    "[taxonomies]",
+    "tags = []",
+    "categories = []",
+    "+++",
+  })
+end, {})
