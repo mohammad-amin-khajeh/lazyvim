63b7beb4b031edce28d5c7de3c3a9c168b21b925
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-30 02:19:53 +0330

    Make rainbow delimiters update to the lastest version

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -2,7 +2,6 @@ return {
   {
     "hiphish/rainbow-delimiters.nvim",
     event = "BufReadPost",
-    commit = "97b8238",
     config = function()
       require("rainbow-delimiters.setup").setup({
         highlight = {
