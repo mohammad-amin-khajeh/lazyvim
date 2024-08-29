2cd3c5df63385e6f5583d15eaf90eb225c09f737
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-08-29 21:13:25 +0330

    Change nvim-lint to dirrectly use the .config directory instead of ~/.config

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,11 +1,11 @@
-local HOME = os.getenv("HOME")
+local CONFIG_DIR = os.getenv("XDG_CONFIG_HOME")
 return {
   {
     "mfussenegger/nvim-lint",
     opts = {
       linters = {
         ["markdownlint-cli2"] = {
-          args = { "--config", HOME .. "/.config/nvim/lua/config/.markdownlint-cli2.yaml", "--" },
+          args = { "--config", CONFIG_DIR .. "/nvim/lua/config/.markdownlint-cli2.yaml", "--" },
         },
       },
     },
