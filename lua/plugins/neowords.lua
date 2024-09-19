e541101182383a054ab3238491faca55c00322a6
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-09-19 19:07:43 +0330

    Add | and & to the list of characters to hop to in neowords.lua

--- lua/plugins/neowords.lua
+++ lua/plugins/neowords.lua
@@ -7,7 +7,7 @@ return {
       local p = neowords.pattern_presets
 
       local subword_hops =
-        neowords.get_word_hops(p.snake_case, p.camel_case, p.upper_case, p.hex_color, "\\v[[:digit:]]+")
+        neowords.get_word_hops(p.snake_case, p.camel_case, p.upper_case, p.hex_color, "\\v[[:digit:]]+", "\\|+", "\\&+")
 
       vim.keymap.set({ "n", "x" }, "w", subword_hops.forward_start)
       vim.keymap.set({ "n", "x" }, "e", subword_hops.forward_end)
