941d01a363c29102263c3eb05ef934fb2fa7a7aa
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-09-28 16:44:29 +0330

    Change & and | in neowords to && and || respectively
    Previously, a single & or | would be counted as a word and hopped to
    which is often not desired, but now only && and || would be seen as
    words(though, even if the number of repetitions is higher than 2 that
    would work too, but I'm fine with it.)

--- lua/plugins/neowords.lua
+++ lua/plugins/neowords.lua
@@ -6,8 +6,15 @@ return {
       local neowords = require("neowords")
       local p = neowords.pattern_presets
 
-      local subword_hops =
-        neowords.get_word_hops(p.snake_case, p.camel_case, p.upper_case, p.hex_color, "\\v[[:digit:]]+", "\\|+", "\\&+")
+      local subword_hops = neowords.get_word_hops(
+        p.snake_case,
+        p.camel_case,
+        p.upper_case,
+        p.hex_color,
+        "\\v[[:digit:]]+",
+        "\\|\\|+",
+        "\\&\\&+"
+      )
 
       vim.keymap.set({ "n", "x" }, "w", subword_hops.forward_start)
       vim.keymap.set({ "n", "x" }, "e", subword_hops.forward_end)
