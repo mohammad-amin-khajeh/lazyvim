169cdf667c908bda943e4e69b77bd2bfea8ed0e4
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-11-26 07:53:35 +0330

    Switch to friendly-snippets

--- lua/plugins/extend_snippets.lua
+++ lua/plugins/extend_snippets.lua
@@ -1,19 +1,20 @@
 return {
-  {
-    "rafamadriz/friendly-snippets",
-    opts = function()
-      require("luasnip").filetype_extend("htmldjango", { "html" })
-      require("luasnip").filetype_extend("python", { "django" })
-      require("luasnip").filetype_extend("typescriptreact", { "html" })
-    end,
-  },
 
   -- {
-  --   "garymjr/nvim-snippets",
-  --   opts = {
-  --     extended_filetypes = {
-  --       typescriptreact = { "html" },
-  --     },
-  --   },
+  --   "rafamadriz/friendly-snippets",
+  --   opts = function()
+  --     require("luasnip").filetype_extend("htmldjango", { "html" })
+  --     require("luasnip").filetype_extend("python", { "django" })
+  --     require("luasnip").filetype_extend("typescriptreact", { "html" })
+  --   end,
   -- },
+
+  {
+    "garymjr/nvim-snippets",
+    opts = {
+      extended_filetypes = {
+        typescriptreact = { "html" },
+      },
+    },
+  },
 }
