51dc83822f5dbe685c64afa95f3f619df3248389
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-11-26 07:54:57 +0330

    reduce and refactor the blink.cmp config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,29 +6,12 @@ return {
     ---@type blink.cmp.Config
     opts = {
       keymap = {
-        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        preset = "enter",
         ["<C-e>"] = { "hide", "fallback" },
-        ["<CR>"] = { "accept", "fallback" },
-
-        ["<Tab>"] = { "snippet_forward", "fallback" },
-        ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        -- ["<C-p>"] = { "select_prev", "fallback" },
-        -- ["<C-n>"] = { "select_next", "fallback" },
-
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
       },
       windows = {
-        autocomplete = {
-          draw = "reversed",
-          direction_priority = { "s" },
-        },
-
         documentation = {
-          auto_show_delay_ms = 300,
+          auto_show_delay_ms = 200,
         },
       },
     },
