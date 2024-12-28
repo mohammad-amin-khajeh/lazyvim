cc32c430e6927af9a6086bfa9f9b02b5ec0613ab
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2024-12-28 09:19:25 +0330

    Fix all terminals floating

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,12 +2,10 @@ return {
   "folke/snacks.nvim",
   ---@type snacks.Config
   opts = {
-    terminal = {
-      ---@class snacks.terminal.Config
-      ---@field win? snacks.win.Config
-      ---@field override? fun(cmd?: string|string[], opts?: snacks.terminal.Opts) Use this to use a different terminal implementation
-
-      win = { position = "float" },
+    styles = {
+      terminal = {
+        position = "float",
+      },
     },
   },
 }
