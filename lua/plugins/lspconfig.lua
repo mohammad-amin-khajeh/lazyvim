33363c5259a2a3e956ac8c7e5e85882d9c53ea80
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-01-15 20:42:17 +0330

    Replace telescope with snacks picker

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,35 +9,4 @@ return {
       },
     },
   },
-
-  {
-    "neovim/nvim-lspconfig",
-    opts = function()
-      -- "reuse_win = true" disables snacks' zen mode so I'm disabling that
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = {
-        "gd",
-        function()
-          require("telescope.builtin").lsp_definitions({ reuse_win = false })
-        end,
-        desc = "Goto Definition",
-        has = "definition",
-      }
-
-      keys[#keys + 1] = {
-        "gI",
-        function()
-          require("telescope.builtin").lsp_implementations({ reuse_win = false })
-        end,
-        desc = "Goto Implementation",
-      }
-      keys[#keys + 1] = {
-        "gy",
-        function()
-          require("telescope.builtin").lsp_type_definitions({ reuse_win = false })
-        end,
-        desc = "Goto T[y]pe Definition",
-      }
-    end,
-  },
 }
